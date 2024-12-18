717fadd02278fa112eedcd4e4cb07a8f7989d24f
samireliasjabib <samir.jabib@arcticgrey.com>
2024-12-17 23:01:15 -0500

    fix minor bug in brand

--- app/components/home/top-cigars-brands/BrandCard.tsx
+++ app/components/home/top-cigars-brands/BrandCard.tsx
@@ -10,16 +10,14 @@ function BrandCard({imageUrl, altText}: BrandCardProps) {
       <div className="absolute bottom-0 left-0 h-[40%] w-full border-b border-l border-gray-200 transition-colors duration-300 group-hover:border-primary" />
       <div className="absolute bottom-0 right-0 h-[40%] w-full border-b border-r border-gray-200 transition-colors duration-300 group-hover:border-primary" />
 
-      <div className="h-14 flex justify-center items-center">
+      <div className="flex justify-center items-center h-full w-hull">
         <Image
           data={{
             url: imageUrl,
             altText,
           }}
-          className="object-contain filter grayscale hover:filter-none transition-all duration-300  transform group-hover:scale-110"
+          className="object-contain h-14 filter grayscale hover:filter-none transition-all duration-300  transform group-hover:scale-110"
           width={110}
-          height={60}
-          aspectRatio="1:1"
         />
       </div>
     </div>
