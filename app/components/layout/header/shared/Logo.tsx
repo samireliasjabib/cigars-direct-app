3acb355acd2909c509adca36830da49934dd03d7
samireliasjabib <samir.jabib@arcticgrey.com>
2024-12-18 22:07:21 -0500

    put link in logo

--- app/components/layout/header/shared/Logo.tsx
+++ app/components/layout/header/shared/Logo.tsx
@@ -1,15 +1,18 @@
 import {Image} from '@shopify/hydrogen';
 import CIGARS_DIRECT_LOGO_WHITE from '../../../../assets/cigar-logo-white.jpg';
+import {Link} from '~/components/shared/Link';
 
 function Logo() {
   return (
-    <Image
-      data={{url: CIGARS_DIRECT_LOGO_WHITE, altText: 'CIGARS DIRECT'}}
-      width={140}
-      height={40}
-      aspectRatio="1:1"
-      className="object-contain"
-    />
+    <Link to="/">
+      <Image
+        data={{url: CIGARS_DIRECT_LOGO_WHITE, altText: 'CIGARS DIRECT'}}
+        width={140}
+        height={40}
+        aspectRatio="1:1"
+        className="object-contain"
+      />
+    </Link>
   );
 }
 
