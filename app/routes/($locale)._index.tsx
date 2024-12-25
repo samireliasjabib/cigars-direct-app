44cd8a88543f29ca6b7d697584214c43874a8c10
samireliasjabib <samir.jabib@arcticgrey.com>
2024-12-25 15:12:21 -0500

    compress banner mobile image

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -79,7 +79,7 @@ export default function Homepage() {
           altText: 'Promotional banner for desktop users showing latest offers',
         }}
         mobileImage={{
-          url: 'https://cdn.shopify.com/s/files/1/0724/4899/9675/files/banner-mobile.webp?v=1734482417',
+          url: 'https://cdn.shopify.com/s/files/1/0724/4899/9675/files/banner_mobile.jpg?v=1735157485',
           altText: 'Promotional banner for mobile users showing latest offers',
         }}
       />
