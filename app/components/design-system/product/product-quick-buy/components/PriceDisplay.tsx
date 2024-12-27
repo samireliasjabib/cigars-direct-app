679da62626123c5a43ed61b5ecdb2a8886efae23
samireliasjabib <samir.jabib@arcticgrey.com>
2024-12-26 19:25:56 -0500

    mynor fix

--- app/components/design-system/product/product-quick-buy/components/PriceDisplay.tsx
+++ app/components/design-system/product/product-quick-buy/components/PriceDisplay.tsx
@@ -1,7 +1,8 @@
 import type {PriceDisplayProps} from '../types';
 
 export function PriceDisplay({price, compareAtPrice}: PriceDisplayProps) {
-  const [dollars, cents] = price.amount.split('.');
+  console.log(price, 'price in price display');
+  const [dollars, cents = '00'] = price.amount.split('.');
 
   return (
     <div className="flex items-center justify-between border-b pb-2">
@@ -9,7 +10,9 @@ export function PriceDisplay({price, compareAtPrice}: PriceDisplayProps) {
       <div className="flex flex-row gap-4 items-center justify-end">
         <span className="text-lg font-semibold text-gray-900">
           ${dollars}
-          <sup className="text-xs ml-0.5">{cents || '00'}</sup>
+          <sup className="text-xs ml-0.5">
+            {cents === '0' ? '00' : cents || '00'}
+          </sup>
         </span>
         {compareAtPrice && (
           <span className="text-lg text-gray-500 line-through">
